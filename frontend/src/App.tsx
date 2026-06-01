37122b136bff98ad4c565320f42d0ebd3ff136df
Nico Gräf <graef.nico@gmail.com>
2026-06-01 13:01:44 +0200

    fix: add tooltip provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import { Outlet } from 'react-router'
 
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
+import { TooltipProvider } from '@/components/ui/tooltip'
 
 export default function App() {
   return (
     <>
       <ThemeProvider storageKey="vite-ui-theme">
-        <Toaster position="top-right" />
-        <Outlet />
+        <TooltipProvider>
+          <Toaster position="top-right" />
+          <Outlet />
+        </TooltipProvider>
       </ThemeProvider>
     </>
   )
