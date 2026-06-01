4cb4c682663f0fc009aee2a46f1482893084d044
Nico Gräf <graef.nico@gmail.com>
2026-06-01 13:10:22 +0200

    fix: down migration schema

--- database/migrations/01_initial.down.sql
+++ database/migrations/01_initial.down.sql
@@ -1,21 +1,25 @@
 BEGIN;
 
 -- Drop in reverse dependency order
-DROP TRIGGER IF EXISTS events_no_truncate ON events;
-DROP TRIGGER IF EXISTS events_no_delete ON events;
-DROP TRIGGER IF EXISTS events_no_update ON events;
-DROP FUNCTION IF EXISTS prevent_event_mutation;
-DROP TABLE IF EXISTS table_state;
-DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS kategorie_drucker;
+DROP TABLE IF EXISTS tisch_favoriten;
+DROP TABLE IF EXISTS tisch_sessions;
+
+DROP TRIGGER IF EXISTS kassenjournal_no_truncate ON kassenjournal;
+DROP TRIGGER IF EXISTS kassenjournal_no_delete ON kassenjournal;
+DROP TRIGGER IF EXISTS kassenjournal_no_update ON kassenjournal;
+DROP FUNCTION IF EXISTS prevent_kassenjournal_mutation;
+DROP TABLE IF EXISTS kassenjournal;
+
+DROP TABLE IF EXISTS kassensitzungen;
 DROP TABLE IF EXISTS produkt_varianten;
 DROP TABLE IF EXISTS produkte;
-DROP TABLE IF EXISTS tisch_favoriten;
 DROP TABLE IF EXISTS tische;
 DROP TABLE IF EXISTS users;
 
 -- Drop types after dropping tables that depend on them
 DROP TYPE IF EXISTS EntityStatus;
 DROP TYPE IF EXISTS UserRole;
-DROP TYPE IF EXISTS ProductCategory;
+DROP TYPE IF EXISTS ProduktKategorie;
 
 COMMIT;
